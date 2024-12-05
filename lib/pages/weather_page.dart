dbc9bb4709f1be4db7c9b035d2508c5a1923946b
Maxx <max13.bl@gmail.com>
2024-12-05 21:11:29 +0100

    search bar added

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -14,6 +14,7 @@ class _WeatherPageState extends State<WeatherPage> {
   bool isLoading = true;
   final _weatherService = WeatherService("154657d107a3b45374b467c0fbad51f4");
   Weather? _weather;
+  final TextEditingController cityController = TextEditingController();
 
   @override
   void initState() {
@@ -29,10 +30,25 @@ class _WeatherPageState extends State<WeatherPage> {
     try {
       final weather = await _weatherService.getWeather(cityName);
       _weather = weather;
-      print(
-          "Weather : ${_weather?.cityName} ${_weather?.temperature} ${_weather?.mainCondition}");
     } catch (e) {
-      print(e);
+      _weather =
+          Weather(cityName: "Not found", temperature: 0, mainCondition: "unknown");
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  _fetchWeatherSearch(String cityName) async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      _weather = weather;
+    } catch (e) {
+      _weather =
+          Weather(cityName: "Not found", temperature: 0, mainCondition: "unknown");
     }
     setState(() {
       isLoading = false;
@@ -42,6 +58,8 @@ class _WeatherPageState extends State<WeatherPage> {
   String getWeatherAnimation(String? mainCondition) {
     if (mainCondition == null) return "assets/sun.json";
     switch (mainCondition.toLowerCase()) {
+      case 'unknown': 
+        return 'assets/unknown.json';
       case 'clouds':
         return 'assets/cloud.json';
       case 'snow':
@@ -70,28 +88,99 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(33, 33, 33, 1),              
-      body: Center(
-          child: isLoading
-              ? const CircularProgressIndicator(color: Colors.white)
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: const Color.fromRGBO(33, 33, 33, 1),
+      body: Stack(
+        children: [
+          Center(
+              child: isLoading
+                  ? const CircularProgressIndicator(color: Colors.white)
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        const Icon(Icons.location_on_sharp, size: 40, color: Colors.grey,),
-                        Text(_weather?.cityName ?? "Error loading city...", style: const TextStyle(fontSize: 24, fontFamily: 'Heavitas', color: Colors.grey)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Column(children: [
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.location_on_sharp,
+                                    size: 36,
+                                    color: Colors.grey,
+                                  ),
+                                  Text(
+                                      _weather?.cityName ??
+                                          "Error loading city...",
+                                      style: const TextStyle(
+                                          fontSize: 24,
+                                          fontFamily: 'Heavitas',
+                                          color: Colors.grey)),
+                                ],
+                              ),
+                              Text("${_weather?.temperature.round()}°C",
+                                  style: const TextStyle(
+                                      fontSize: 32,
+                                      fontFamily: 'Heavitas',
+                                      color: Colors.grey)),
+                            ])
+                          ],
+                        ),
+                        Lottie.asset(
+                            getWeatherAnimation(_weather?.mainCondition)),
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 40),
+                          child: Text("${_weather?.mainCondition}",
+                            style: const TextStyle(
+                                fontSize: 32,
+                                fontFamily: 'Heavitas',
+                                color: Colors.grey)),
+                        )
                       ],
-                    ),
-                    Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
-                    Text("${_weather?.temperature.round()}°C", style: const TextStyle(fontSize: 32, fontFamily: 'Heavitas', color: Colors.grey)),
-                  ],
-                )),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor:Colors.black,
-        onPressed: _fetchWeather,
-        child: const Icon(Icons.refresh, color: Colors.grey,),
+                    )),
+          Positioned(
+            bottom: 20,
+            left: 32,
+            child: Row(
+              children: [
+                Container(
+                  width: 260,
+                  height: 50,
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 16, top: 3),
+                    child: TextField(
+                    controller: cityController,
+                    decoration: const InputDecoration(
+                        hintText: 'Enter city name',
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(fontFamily: 'Heavitas')),
+                    style: const TextStyle(
+                        fontFamily: 'Heavitas', color: Colors.grey),
+                    onSubmitted: (value) async {
+                      _fetchWeatherSearch(cityController.text);
+                    },
+                  ),
+                  )
+                ),
+                const SizedBox(
+                    width: 16), // Add space between the search bar and button
+                FloatingActionButton(
+                  backgroundColor: Colors.black,
+                  onPressed: _fetchWeather,
+                  child: const Icon(
+                    Icons.refresh,
+                    size: 32,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,13 +6,13 @@ import 'package:http/http.dart' as http;
 
 class WeatherService {
 
-  static const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+  static const baseUrl = "http://api.openweathermap.org/data/2.5/weather";
   final String apiKey;
 
   WeatherService(this.apiKey);
 
   Future <Weather> getWeather(String cityName) async {
-    final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+    final response = await http.get(Uri.parse('$baseUrl?q=$cityName&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
@@ -25,16 +25,18 @@ class WeatherService {
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
+      return "Not found";
+    } else {
+      Position position = await Geolocator.getCurrentPosition(      
+        locationSettings: const LocationSettings(accuracy: LocationAccuracy.high),
+      );
+
+      List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+
+      String? city = placemarks[0].locality;
+
+      return city ?? "";
+
     }
-
-    Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high
-    );
-
-    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
-
-    String? city = placemarks[0].locality;
-
-    return city ?? "";
   }
 }
